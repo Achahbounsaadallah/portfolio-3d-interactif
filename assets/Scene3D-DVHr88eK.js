f805151440532aa7be866d4a364979454c53ffd2
Achahboun Saad Allah <achahbounsaadallah@gmail.com>
2026-04-06 09:44:41 +0100

    Updates

--- assets/Scene3D-DVHr88eK.js
+++ assets/Scene3D-DVHr88eK.js
@@ -1,4 +1,4 @@
-import{r as Ue,g as dx,j as xt}from"./index-nDjKL_Os.js";/**
+import{r as Ue,g as dx,j as xt}from"./index-gX4E34w4.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
