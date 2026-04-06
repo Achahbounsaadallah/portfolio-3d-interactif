d74e4da3219fa86e7dfb6cf9d932db14c742009a
Achahboun Saad Allah <achahbounsaadallah@gmail.com>
2026-04-06 09:42:30 +0100

    deploiement version

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter  basename="/portfolio-3d-interactif">
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Index />} />
